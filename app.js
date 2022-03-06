f7749e736ee6a9c2426113dd13b12fdefb166985
Xander999 <sahasuraj420@gmail.com>
2022-03-06 08:31:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AlienDBex'
+
+const app = express()
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', function(){
+    console.log('Connected ...')
+})
+
+const alienRouter = require('./routers/alien')
+app.use('/aliens',alienRouter)
+
+app.listen(9000, function(){
+    console.log('Server Started ....')
+})
